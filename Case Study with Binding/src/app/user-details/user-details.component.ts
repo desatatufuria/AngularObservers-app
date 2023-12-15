3a039071b6f81a30506f49533517abc471b6a49a
Juan Carlos Linaje <43032811+desatatufuria@users.noreply.github.com>
2023-12-15 12:02:24 +0100

    Delete Case Study with Binding directory

--- Case Study with Binding/src/app/user-details/user-details.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-user-details',
-  templateUrl: 'user-details.component.html',
-})
-export class UserDetailsComponent {
-  @Input() user: string | undefined;
-}
